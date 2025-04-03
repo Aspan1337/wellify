b8307c6252704a6fff99bb726db4994f9e3d3899
Ваше Имя <your-email@example.com>
2025-04-04 01:13:32 +0500

    text

---
+++ src/components/FeedbackModal/telegramConfig.jsx
@@ -0,0 +1,4 @@
+export const TELEGRAM_BOT_TOKEN = "8175939498:AAHcPBvKFlQ5FYa44h4yjF5mml4xy3ZOlok";
+export const TELEGRAM_CHAT_ID = "-4668877053";
+
+export const TELEGRAM_API_URL = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
